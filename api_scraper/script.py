a37fcb9a52894a124c9d873a95dea34523ef3661
_MR1_ <96888594+Marouane-Elgoumiri@users.noreply.github.com>
2024-08-20 00:32:56 +0100

    created the xls

--- api_scraper/script.py
+++ api_scraper/script.py
@@ -18,6 +18,20 @@ def get_job_postings():
     response = requests.get(url=BASE_URL, headers=REQUEST_HEADER)
     return response.json()
 
+def output_job_to_xls(data):
+    wb = Workbook()
+    job_sheet = wb.add_sheet("Jobs")
+    headers = list(data[0].keys())
+    for i in range(0, len(headers)):
+        job_sheet.write(0, i, headers[i])
+    for i in range(0, len(data)):
+        job = data[i]
+        values = list(job.values())
+        for x in range(0, len(values)):
+            job_sheet.write(i+1, x, values[x])
+
+    wb.save("Remote_jobs.xls")
+
 if __name__ == "__main__":
-    json = get_job_postings()[1]
-    print(json)
+    json = get_job_postings()[1:]
+    output_job_to_xls(json)
