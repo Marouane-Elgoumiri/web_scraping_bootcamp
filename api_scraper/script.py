391c015cfd4c0cbc5c830cac2d433a3af1794f12
_MR1_ <96888594+Marouane-Elgoumiri@users.noreply.github.com>
2024-08-19 21:37:29 +0100

    API fetched successfully

--- api_scraper/script.py
+++ api_scraper/script.py
@@ -8,5 +8,16 @@ from email.mime.application import MIMEApplication
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
 
+BASE_URL = "https://remoteok.com/api"
+USER_AGENTS = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0"
+REQUEST_HEADER = {
+    'User-Agent': USER_AGENTS,
+    'Accept-Language':'en-US, en;q=0.5',
+}
+def get_job_postings():
+    response = requests.get(url=BASE_URL, headers=REQUEST_HEADER)
+    return response.json()
+
 if __name__ == "__main__":
-    print("hello world!")
+    json = get_job_postings()[1]
+    print(json)
